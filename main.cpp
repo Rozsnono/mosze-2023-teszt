8a97f46df8c63d9a36237a2b5b600fddbfd3c4c7
rozsnono <rozsnono@gmail.com>
2024-09-25 12:52:26 +0200

    Comment errors

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //ERROR N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa' //ERROR -> Missing ;
+    for (int i = 0;) //ERROR -> Wrong for loop
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //ERROR -> Wrong for loop
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //ERROR -> Missing ; and value
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) //ERROR -> Wrong for loop
     {
-        atlag += b[i]
+        atlag += b[i] //ERROR -> Missing ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
